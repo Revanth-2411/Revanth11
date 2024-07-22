e70716e21e2c3359efef8f9d0a38273a2bd81432
Revanth-2411 <chalapatirevath2006@gmail.com>
2024-07-22 21:21:35 +0530

    Add files via upload

---
+++ Revanth.java
@@ -0,0 +1,9 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.io.*;
+import java.lang.*;
+class Revanth  {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ Student.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.lang.*;
+class Student{
+    public static void main(String args[])
+    {
+        int a=10,b=20;
+        int result=a+b;
+        System.out.println("Result="+result);
+    }
+}
