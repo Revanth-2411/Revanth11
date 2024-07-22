4cc9d8f4292ca2681c16bbe55c6b7d0ff5daa279
Revanth-2411 <chalapatirevath2006@gmail.com>
2024-07-22 21:19:08 +0530

    java

---
+++ Jhony.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.lang.*;
+class Jhony{
+    public static void main(String args[])
+    {
+        int rollnumber=11;
+        String name="Revanth";
+        char grade='A';
+        double cgpa=9.2;
+        long l=12344456;
+        char ch1=69;
+        System.out.println("char"+ch1);
+        System.out.println("roll"+rollnumber);
+        System.out.println("name"+name);
+        System.out.println("grade"+grade);
+        System.out.println("cgpa"+cgpa);
+        System.out.println("long"+l);
+    }
+}
---
+++ Naga.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.io.*;
+import java.lang.*;
+class Naga  {
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("3+2");
+        System.out.println(3+2);
+        System.out.println(3>=2);
+    }
+}
